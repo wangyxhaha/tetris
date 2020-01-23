19168e255afbce34fff18566d7dd5b659a580a7f
wangyxhaha <wyx12345haha@163.com>
2020-01-23 23:16:27 +0800

    添加头文件

---
+++ hf/alg.h
@@ -0,0 +1,20 @@
+/*
+* made by 王宇轩 
+* 需要ege图形库 
+*/
+
+/*标题宏*/
+#define start "tetris-start"
+#define menu "tetris-menu"
+/**/
+
+/*函数*/
+void my_init(){
+	setinitmode(0); //去除开头动画 
+	return;
+}
+
+void start_ani();
+
+#include "start.cpp"
+
