ea83cd879864c19b3f785a19f3246e8fa86bb102
wangyxhaha <wyx12345haha@163.com>
2020-01-23 23:17:08 +0800

    添加了头文件和字体

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
+/*
+* made by 王宇轩 
+* 需要ege图形库 
+* 需要编译指令
+* "-lgraphics64 -lgdi32 -limm32 -lmsimg32 -lole32 -loleaut32 -lwinmm -luuid -mwindows"
+*/
 #include <iostream>
+#include <graphics.h>
+#include "./hf/alg.h"
+#define chx 640
+#define chy 640
 using namespace std;
 
 int main(){
-	;
+	my_init();
+	initgraph(chx,chy); //打开窗口 
+	setcaption(menu);
+	getch();
+	closegraph(); //关闭窗口 
 }
 
